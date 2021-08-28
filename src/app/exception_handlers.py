09f76152baa73aa7c61689cc30154926abc50c74
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-08-29 01:28:09 +0300

    Refactoring

--- src/app/exception_handlers.py
+++ src/app/exception_handlers.py
@@ -4,6 +4,22 @@ from app.exceptions import Errno
 from app.response import response_err
 
 
+def extract_err_data_from_exc(exc):
+    if isinstance(exc.detail, dict) and 'detail' in exc.detail:
+        return exc.detail['detail'], None
+
+    if isinstance(exc.detail, dict):
+        invalid_fields = {}
+        for field in exc.detail:
+            invalid_fields[field] = ', '.join(exc.detail[field])
+        return 'bad request', invalid_fields
+
+    if isinstance(exc.detail, list):
+        return ', '.join(exc.detail), None
+
+    return exc.detail, None
+
+
 def default_exception_handler(exc, context):
     if issubclass(type(exc), Errno) or isinstance(exc, Errno):
         return response_err(exc.code, exc.msg, http_code=exc.http_code)
@@ -15,25 +31,16 @@ def default_exception_handler(exc, context):
     if response is None:
         return None
 
-    data = {
+    msg, invalid_fields = extract_err_data_from_exc(exc)
+    response.data = {
         'status': 'ERR',
         'error': {
             'code': exc.status_code or 500,
-            'msg': 'unexpected error',
+            'msg': msg or 'unexpected error',
         },
     }
 
-    if isinstance(exc.detail, dict):
-        data['error']['msg'] = 'bad request'
-        data['error']['invalid_fields'] = {}
-        for field in exc.detail:
-            data['error']['invalid_fields'][field] = ', '.join(exc.detail[field])
-
-    elif isinstance(exc.detail, list):
-        data['error']['msg'] = ', '.join(exc.detail)
-    else:
-        data['error']['msg'] = exc.detail
-
-    response.data = data
+    if invalid_fields:
+        response.data['error']['invalid_fields'] = invalid_fields
 
     return response
