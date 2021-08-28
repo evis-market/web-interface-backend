ffa68279250bd89af8a0347d2808a9ab60ed817a
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-08-29 00:37:29 +0300

    Change status field to http_code

--- src/app/response.py
+++ src/app/response.py
@@ -1,19 +1,19 @@
 from rest_framework.response import Response
 
 
-def response_ok(data, status=None, template_name=None, headers=None, content_type=None):
+def response_ok(data, http_code=None, template_name=None, headers=None, content_type=None):
     if not data:
         data = {}
 
     data['status'] = 'OK'
-    return Response(data, status=status, template_name=template_name, headers=headers, content_type=content_type)
+    return Response(data, status=http_code, template_name=template_name, headers=headers, content_type=content_type)
 
 
-def response_err(code: int, msg: str, status=None, template_name=None, headers=None, content_type=None):
+def response_err(code: int, msg: str, http_code=None, template_name=None, headers=None, content_type=None):
     data = {
         'status': 'ERR',
         'error': {
-            'code': status or 500,
+            'code': http_code or 500,
             'msg': 'unexpected error',
         },
     }
@@ -24,4 +24,4 @@ def response_err(code: int, msg: str, status=None, template_name=None, headers=N
     if msg:
         data['error']['msg'] = msg
 
-    return Response(data, status=status, template_name=template_name, headers=headers, content_type=content_type)
+    return Response(data, status=http_code, template_name=template_name, headers=headers, content_type=content_type)
