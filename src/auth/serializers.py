6ef80f54123f53647c65743938b7740d35c8d89c
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-08-29 12:33:27 +0300

    Set required to False for grant_type

--- src/auth/serializers.py
+++ src/auth/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 
 class GrantTokenSerializer(serializers.Serializer):
-    grant_type = serializers.CharField(required=True)
+    grant_type = serializers.CharField(required=False)
     login = serializers.CharField(required=False)
     password = serializers.CharField(required=False)
     refresh_token = serializers.CharField(required=False)
