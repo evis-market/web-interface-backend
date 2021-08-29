109d692b8e7305f2583cd16a0c3303108b7523e9
Alexandr Valov <alexandr@springdel.com>
2021-08-29 16:22:17 +0300

    EB-14
    - added new command for console to get user token: "./manage.py grant email/phone"
    - added new function to get user by login

---
+++ src/auth/management/commands/jwt.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from auth.service import JWTAuthService
+from users.managers import UserManager
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = 'Get user token'
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            'grant',
+            nargs='+',
+            type=str,
+            help='Find user by email/phone/erc20',
+        )
+
+    def handle(self, *args, **options):
+        if options['grant']:
+            try:
+                user = UserManager.get_by_login(options['grant'][1])
+            except User.DoesNotExist:
+                raise CommandError(f'User does not exist')
+            if user.is_superuser:
+                print(JWTAuthService.get_tokens_for_user(user))
+            else:
+                raise CommandError(f'User is not a superuser')
+
+        self.stdout.write(self.style.SUCCESS('Successfully received a tokens'))
