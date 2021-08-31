a319a9b078a1c7cea3df85d4a8a03796b9c3fda2
Vorontsov Alexey <avorontsov@uma.tech>
2021-08-31 09:23:30 +0300

    EB-4

--- src/categories/models.py
+++ src/categories/models.py
@@ -5,6 +5,16 @@ from django.utils.text import slugify
 from categories.managers import CategoryManager
 
 
+class RecommendedFor(models.Model):
+    for_whom = models.CharField('For whom is recommended', max_length=50, db_index=True, unique=True)
+
+    class Meta:
+        db_table = 'recommended_for'
+
+    def __str__(self):
+        return self.for_whom
+
+
 class Category(mptt_models.MPTTModel):
     name = models.CharField('Name', max_length=190, db_index=True, unique=True)
     descr = models.TextField('Description', blank=True, null=False, default='')
@@ -12,7 +22,9 @@ class Category(mptt_models.MPTTModel):
     slug = models.SlugField('Slug', max_length=190, blank=True, db_index=True, unique=True)
     sort_id = models.PositiveIntegerField('Ordering', blank=True, default=2147483647)
     parent = mptt_models.TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
-
+    recommended_for = models.ManyToManyField(
+        RecommendedFor, blank=True, related_name='categories', verbose_name='For whom category is recommended',
+    )
     objects = CategoryManager()
 
     def __str__(self):
--- src/categories/serializers.py
+++ src/categories/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from categories.models import Category
+from categories.models import Category, RecommendedFor
+
+
+class RecommendedForSerializer(serializers.ModelSerializer):
+    def to_representation(self, value):
+        return value.for_whom
+
+    class Meta:
+        model = RecommendedFor
+        fields = ['for_whom']
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    recommended_for = RecommendedForSerializer(read_only=True, many=True)
+
     class Meta:
         model = Category
-        fields = ['id', 'parent_id', 'name', 'descr', 'logo_url', 'slug', 'sort_id']
+        fields = ['id', 'parent_id', 'name', 'descr', 'logo_url', 'slug', 'sort_id', 'recommended_for']
