32aca0aff698b71d062778e032b4b7f14bf89ef6
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-04 16:21:30 +0300

    Move admin url down

--- src/app/urls.py
+++ src/app/urls.py
@@ -22,8 +22,8 @@ from django.urls import include, path
 urlpatterns = [
     path('api/v1/auth/', include('auth.urls')),
     path('api/v1/categories/', include('categories.urls')),
-    path('admin/', admin.site.urls),
     path('api/v1/sellers/', include('sellers.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
