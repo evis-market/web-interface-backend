9bd4eb664dbb48c6b2207509cde958ef489d490a
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-04 16:21:10 +0300

    Change deps files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install --upgrade pip
-pip3 install -r requirements.txt.in -r requirements.dev.txt.in
+pip3 install -r requirements.txt -r requirements.dev.txt
 #./manage.py migrate
 #./manage.py createsuperuser
 #./manage.py loaddata
