8a9c65d02ddcd629b4501db95bb4db0055a2497d
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-05 22:16:18 +0300

    Fix linter warnings

--- src/app/conf/log.py
+++ src/app/conf/log.py
@@ -1,24 +1,25 @@
 from app.conf.environ import env
 
+
 if env('DEBUG', cast=bool):
     LOGGING = {
         'version': 1,
         'filters': {
             'require_debug_true': {
                 '()': 'django.utils.log.RequireDebugTrue',
-            }
+            },
         },
         'handlers': {
             'console': {
                 'level': 'DEBUG',
                 'filters': ['require_debug_true'],
                 'class': 'logging.StreamHandler',
-            }
+            },
         },
         'loggers': {
             'django.db.backends': {
                 'level': 'DEBUG',
                 'handlers': ['console'],
-            }
-        }
+            },
+        },
     }
