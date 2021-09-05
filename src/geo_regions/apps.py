ad808b699b06667f2b8a079571a43cfd3dae64f9
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-05 13:42:40 +0300

    Change app name from reference to geo_regions

--- src/geo_regions/apps.py
+++ src/geo_regions/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class ReferenceConfig(AppConfig):
+class GeoRegionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'geo_regions'
+    verbose_name = 'Geo regions'
