0c81e4f3f4e07e2a03f188801d9b90982baa324e
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-09-12 14:19:31 +0300

    Uncomment migrate, createsuperuser, loaddata

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt -r requirements.dev.txt
-#./manage.py migrate
-#./manage.py createsuperuser
-#./manage.py loaddata
+./manage.py migrate
+./manage.py createsuperuser
+./manage.py loaddata initial.json
