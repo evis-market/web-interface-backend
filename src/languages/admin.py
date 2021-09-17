c399d5dc2c4141f668ca81f34f38df3f119d18ee
kodsurfer <kodi2020@ya.ru>
2021-09-17 04:38:56 +0300

    update languages admin.py

--- src/languages/admin.py
+++ src/languages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from languages.models import Language
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name_native', 'name_en', 'slug')
+    search_fields = ('name_en', 'slug')
+
+
+admin.site.register(Language, LanguageAdmin)
