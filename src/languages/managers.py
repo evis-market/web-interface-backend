591f4e7f3856c57ee381049ccd8fb23814ae008f
kodsurfer <kodi2020@ya.ru>
2021-09-18 03:32:25 +0300

    update docstrings in views.py & delete get_all_children in managers.py

--- src/languages/managers.py
+++ src/languages/managers.py
@@ -3,6 +3,5 @@ from django.db import models
 
 class LanguageManager(models.Manager):
 
-    def get_all_children(self, pk):
-        language = self.model.objects.get(pk=pk)
-        return language.get_descendants()
+    def get_all(self):
+        return self.model.objects.all()
