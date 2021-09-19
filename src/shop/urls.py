05a05660cf1731738d752cd1270d93fa170b5068
Evgeny Mamonov <46217874+evgeny-mamonov@users.noreply.github.com>
2021-09-19 19:41:36 +0300

    Fix trailing slash

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -8,7 +8,7 @@ from shop.views import (
 urlpatterns = [
     path('categories/', ProductCategoriesListView.as_view(), name='CategoriesNotEmpty'),
     path('products/', ProductsListView.as_view(), name='Products'),
-    path('product/<int:seller_product_id>/', ProductDetailView.as_view(), name='ProductDetail'),
-    path('related_products/<int:seller_product_id>/', RelatedProductsListView.as_view(), name='RelatedProducts'),
-    path('all_product_options/', ProductOptionsListView.as_view(), name='RelatedProducts'),
+    path('product/<int:seller_product_id>', ProductDetailView.as_view(), name='ProductDetail'),
+    path('related_products/<int:seller_product_id>', RelatedProductsListView.as_view(), name='RelatedProducts'),
+    path('all_product_options', ProductOptionsListView.as_view(), name='RelatedProducts'),
 ]
