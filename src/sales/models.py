ec099b65d9e96f422a3e71945af7599eecdade76
Evgeny Mamonov <46217874+evgeny-mamonov@users.noreply.github.com>
2021-09-22 21:05:25 +0300

    Update models.py
    Fix model fields

--- src/sales/models.py
+++ src/sales/models.py
@@ -7,9 +7,9 @@ from sales.managers import SaleManager
 class Sale(models.Model):
     created_at = models.DateTimeField('Created', auto_now_add=True)
     uuid = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4)
-    seller_user_id = models.IntegerField()
-    buyer_user_id = models.IntegerField()
-    amount = MoneyField(max_digits=14, decimal_places=2)
+    seller = models.OneToOneField('sellers.Seller', related_name='Seller', on_delete=models.CASCADE)
+    buyer = models.OneToOneField('users.User', related_name='Buyer', on_delete=models.CASCADE)
+    amount = MoneyField(max_digits=14, decimal_places=6)
 
     objects = SaleManager()
 
@@ -24,8 +24,8 @@ class Sale(models.Model):
 
 
 class SaleProduct(models.Model):
-    sale_id = models.ForeignKey('Sale', related_name='SaleProduct', on_delete=models.SET_NULL, null=True)
-    seller_product_archive_id = models.ForeignKey('seller_products.SellerProductArchive', related_name='SaleProduct', on_delete=models.SET_NULL, null=True)
+    sale = models.ForeignKey('Sale', related_name='SaleProduct', on_delete=models.CASCADE)
+    seller_product_archive_id = models.ForeignKey('seller_products.SellerProductArchive', related_name='SaleProduct', on_delete=models.CASCADE)
 
     objects = SaleManager()
 
