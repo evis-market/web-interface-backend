c8a37c048ffcdffc85edb0d1501bd53d8cd56e8a
kodsurfer <kodi2020@ya.ru>
2021-09-23 01:30:30 +0300

    update sales managers

--- src/sales/managers.py
+++ src/sales/managers.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class SaleManager(models.Manager):
 
-    def list_by_seller_id(self, seller_id):
-        return self.model.objects.filter(seller_user_id=seller_id)
+    def list_by_seller_id(self, seller):
+        return self.model.objects.select_related('seller').filter(seller=seller)
 
-    def list_by_buyer_id(self, buyer_id):
-        return self.model.objects.filter(buyer_user_id=buyer_id)
+    def list_by_buyer_id(self, buyer):
+        return self.model.objects.select_related('users').filter(user=buyer)
 
 
 class SaleProductManager(models.Manager):
