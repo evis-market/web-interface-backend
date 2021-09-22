24db40ec8b09946780a411c38b0a0db0c93c340b
kodsurfer <kodi2020@ya.ru>
2021-09-23 01:20:35 +0300

    add sales admin.py

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from sales.models import Sale
+
+
+class SalesAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'uuid', 'seller', 'buyer', 'amount')
+    search_fields = ('created_at', 'seller', 'buyer', 'amount')
+
+
+admin.site.register(Sale, SalesAdmin)
