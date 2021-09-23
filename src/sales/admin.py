40ed5adfb452f161b92dc1d3b3bde76a596792a2
Evgeny Mamonov <46217874+evgeny-mamonov@users.noreply.github.com>
2021-09-23 19:04:09 +0300

    Update admin.py
    Remove created_at from search_fields

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -5,7 +5,7 @@ from sales.models import Sale
 
 class SalesAdmin(admin.ModelAdmin):
     list_display = ('created_at', 'uuid', 'seller', 'buyer', 'amount')
-    search_fields = ('created_at', 'seller', 'buyer', 'amount')
+    search_fields = ('seller', 'buyer', 'amount')
 
 
 admin.site.register(Sale, SalesAdmin)
