36ef04f83bc1ed36ac3b0ee3c321becf0776bed2
kodsurfer <kodi2020@ya.ru>
2021-09-30 01:39:09 +0300

    uodate user signup validation

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -8,6 +8,8 @@ from app import exceptions
 EMAIL_PATTERN = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 WALLET_ERC_20_PATTERN = r'^0x[a-fA-F0-9]{40}$'
 MIN_PASSWORD_LENGTH = 8
+MIN_PHONE_LENGTH = 11
+MAX_PHONE_LENGTH = 15
 
 
 class SignupRequestSerializer(serializers.ModelSerializer):
@@ -24,8 +26,8 @@ class SignupRequestSerializer(serializers.ModelSerializer):
             raise exceptions.BadRequest('The email is Invalid')
         if len(self.data['password']) < MIN_PASSWORD_LENGTH:
             raise exceptions.BadRequest(f'The password should be {MIN_PASSWORD_LENGTH} or more symbols')
-        if not self.data['phone'].isdigit() or len(self.data['phone']) < 11 or len(self.data['phone']) > 15:
-            raise exceptions.BadRequest(f'The phone should be only digits with length from 11 to 15')
+        if not self.data['phone'].isdigit() or len(self.data['phone']) < MIN_PHONE_LENGTH or len(self.data['phone']) > MAX_PHONE_LENGTH:
+            raise exceptions.BadRequest(f'The phone should be only digits with length from {MIN_PHONE_LENGTH} to {MAX_PHONE_LENGTH}')
         if not re.fullmatch(WALLET_ERC_20_PATTERN, self.data['wallet_for_payments_erc20']):
             raise exceptions.BadRequest('The wallet ERC-20 is Invalid')
 
