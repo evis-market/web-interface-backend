43033f63a66aae33106b06f20699f62dbd808c4d
Evgeny Mamonov <46217874+evgeny-mamonov@users.noreply.github.com>
2021-10-07 23:37:41 +0300

    Fix docstrings

--- src/auth/serializers.py
+++ src/auth/serializers.py
@@ -3,21 +3,14 @@ from rest_framework import serializers
 
 class GrantTokenSerializer(serializers.Serializer):
     """
-        Class representing token serializer
+    Class representing token serializer
 
-        ...
-        Attributes
-        ----------
-        grant_type : CharField
-            grant type, not required
-        login : CharField
-            login, not required
-        password : CharField
-            password, not required
-        refresh_token : CharField
-            refresh token, not required
-
-        """
+    Attributes:
+    grant_type (str): token grant type, required, valid values: password, refresh_token
+    login (str): login, required for grant type "password"
+    password (str): password, required for grant type "password"
+    refresh_token (str): refresh token, required for grant type "refres_token"
+    """
     grant_type = serializers.CharField(required=False)
     login = serializers.CharField(required=False)
     password = serializers.CharField(required=False)
