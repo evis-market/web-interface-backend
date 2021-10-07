563517439d1d682f326db762b2e175eef377992d
kodsurfer <kodi2020@ya.ru>
2021-10-07 19:34:53 +0300

    add docstrings to serializers.py

--- src/auth/serializers.py
+++ src/auth/serializers.py
@@ -2,6 +2,22 @@ from rest_framework import serializers
 
 
 class GrantTokenSerializer(serializers.Serializer):
+    """
+        Class representing token serializer
+
+        ...
+        Attributes
+        ----------
+        grant_type : CharField
+            grant type, not required
+        login : CharField
+            login, not required
+        password : CharField
+            password, not required
+        refresh_token : CharField
+            refresh token, not required
+
+        """
     grant_type = serializers.CharField(required=False)
     login = serializers.CharField(required=False)
     password = serializers.CharField(required=False)
