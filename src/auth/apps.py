b14b7290cd606b8845934fc436f188992c71aedf
Evgeny Mamonov <46217874+evgeny-mamonov@users.noreply.github.com>
2021-10-07 23:03:38 +0300

    Remove docstrings

--- src/auth/apps.py
+++ src/auth/apps.py
@@ -2,20 +2,6 @@ from django.apps import AppConfig
 
 
 class AuthConfig(AppConfig):
-    """
-        Class representing authorization and its configuration
-
-        ...
-        Attributes
-        ----------
-        default_auto_field : str
-            default auto field
-        name : str
-            authorization name
-        label : str
-            label of authorization
-
-        """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth'
     label = 'app_auth'
