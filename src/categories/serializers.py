60cdd30e487bca98e913b4103b53184be3159697
kodsurfer <kodi2020@ya.ru>
2021-10-11 01:29:05 +0300

    add docstrings to categories serializers.py

--- src/categories/serializers.py
+++ src/categories/serializers.py
@@ -4,14 +4,32 @@ from categories.models import Category, RecommendedFor
 
 
 class RecommendedForSerializer(serializers.ModelSerializer):
+    """ Class representing recommendation for serializer """
     class Meta:
+        """ Meta class for class representing recommendation for serializer
+
+        Attributes:
+            model (models.Model): data model
+            fields (list): class fields list
+        """
         model = RecommendedFor
         fields = ['id', 'name']
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """ Class representing category serializer
+
+    Attributes:
+        recommended_for (serializers.ModelSerializer): recommendation for serializer
+    """
     recommended_for = RecommendedForSerializer(read_only=True, many=True)
 
     class Meta:
+        """ Meta class for class representing category serializer
+
+        Attributes:
+            model (models.Model): data model
+            fields (list): class fields list
+        """
         model = Category
         fields = ['id', 'parent_id', 'name', 'descr', 'logo_url', 'slug', 'sort_id', 'recommended_for']
