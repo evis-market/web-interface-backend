3ccb7e914663de4b23bf12a35586bc821e82734a
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-10-29 02:04:43 +0300

    Add id to admin list_fields

--- src/users/admin.py
+++ src/users/admin.py
@@ -5,8 +5,8 @@ from users.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'wallet_erc20')
-    search_fields = ('first_name', 'last_name', 'email', 'phone', 'wallet_erc20')
+    list_display = ('id', 'first_name', 'last_name', 'email', 'phone', 'wallet_erc20')
+    search_fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'wallet_erc20')
     list_filter = (
         ('is_superuser', DropdownFilter),
     )
