4c05125daeccf7f1973f515cdf1403b0f9750c6d
Evgeny A. Mamonov <evgeny@mamonov.org>
2021-11-01 00:44:17 +0300

    Serializers refactoring

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -5,24 +5,53 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from app import exceptions
-from app.validators import PhoneValidator, ERC20Validator
+from app.validators import PhoneValidator, ERC20Validator, PasswordValidator
 from users import models
 from users.models import User
 
 
+LOGIN_NOT_SET_ERR_MSG = 'At least one of the fields: email, phone or wallet_erc20 should be specified'
 
-class SignupRequestSerializer(serializers.ModelSerializer):
+def is_login_field_exists(data):
+    if 'email' in data and data['email']:
+        return True
+
+    if 'phone' in data and data['phone']:
+        return True
+
+    if 'wallet_erc20' in data and data['wallet_erc20']:
+        return True
+
+    return False
+
+
+class SignupRequestSerializer(serializers.Serializer):
     first_name = serializers.CharField(required=False)
     last_name = serializers.CharField(required=False)
     phone = serializers.CharField(required=False, validators=[PhoneValidator])
     email = serializers.EmailField(required=False, validators=[EmailValidator])
     wallet_erc20 = serializers.CharField(required=False, validators=[ERC20Validator])
-    password = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, validators=[PasswordValidator])
 
     def validate(self, data):
-        if not (data['email'] and data['phone'] and data['wallet_erc20']):
-            raise ValidationError('At least one of the fields: email, phone or wallet_erc20 should be specified')
+        if not is_login_field_exists(data):
+            raise exceptions.BadRequest(LOGIN_NOT_SET_ERR_MSG)
+
+        if 'phone' in data:
+            if User.objects.filter(phone=data['phone']).count() > 0:
+                raise ValidationError({"phone": "user with this phone already exists"})
+
+        if 'email' in data:
+            if User.objects.filter(email=data['email']).count() > 0:
+                raise ValidationError({"email": "user with this email already exists"})
+
+        if 'wallet_erc20' in data:
+            if User.objects.filter(wallet_erc20=data['wallet_erc20']).count() > 0:
+                raise ValidationError({"wallet_erc20": "user with this wallet already exists"})
+
+
         return data
+
 
 
 class SendConfirmationEmailRequestSerializer(serializers.Serializer):
@@ -35,12 +64,8 @@ class ConfirmEmailViewSerializer(serializers.Serializer):
 
 
 class SetPasswordBySecretCodeRequestSerializer(serializers.Serializer):
-    password = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, validators=[PasswordValidator])
     secret_code = serializers.CharField(required=True)
-
-    def is_valid(self, raise_exception=False):
-        super().is_valid(raise_exception)
-        password_validation.validate_password(self.data['password'])
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -52,20 +77,16 @@ class UserProfileSerializer(serializers.ModelSerializer):
 class UserProfileUpdateSerializer(serializers.Serializer):
     first_name = serializers.CharField(required=False)
     last_name = serializers.CharField(required=False)
-    phone = serializers.CharField(required=False)
-    email = serializers.EmailField(required=False)
-    wallet_erc20 = serializers.CharField(required=False)
+    phone = serializers.CharField(required=False, validators=[PhoneValidator])
+    email = serializers.EmailField(required=False, validators=[EmailValidator])
+    wallet_erc20 = serializers.CharField(required=False, validators=[ERC20Validator])
 
     def validate(self, data):
-        if not (data['email'] and data['phone'] and data['wallet_erc20']):
-            raise ValidationError('At least one of the fields: email, phone or wallet_erc20 should be specified')
+        if not is_login_field_exists(data):
+            raise exceptions.BadRequest(LOGIN_NOT_SET_ERR_MSG)
+
         return data
 
 
 class UserPasswordUpdateSerializer(serializers.Serializer):
-    password = serializers.CharField(required=True)
-
-    def validate(self, data):
-        password = data.get('password')
-        password_validation.validate_password(password)
-        return data
+    password = serializers.CharField(required=True, validators=[PasswordValidator])
