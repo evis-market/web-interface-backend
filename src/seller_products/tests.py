b1512c762e3c9b193e7d9cf7a74a0574129a0529
AlexandrValov <alexandr@springdel.com>
2021-11-11 22:08:36 +0300

    [EB-65] Add tests for seller-products

--- src/seller_products/tests.py
+++ src/seller_products/tests.py
@@ -2,24 +2,19 @@ import json
 
 import pytest
 from django.core.files import File
+from django.forms import model_to_dict
 from django.urls import reverse
 from mixer.backend.django import mixer
 from rest_framework.test import APIRequestFactory, force_authenticate
 
 from seller_products.models import SellerProduct, SellerProductDataSample, SellerProductDataUrl, SellerProductDataSampleArchive, SellerProductDataUrlArchive, \
     SellerProductArchive
-from seller_products.views import SellerProductsListView
+from seller_products.views import SellerProductsListView, SellerProductsView
 from sellers.models import Seller
 from upload.models import UploadedFile
 from users.models import User
 
 pytestmark = pytest.mark.django_db
-
-"""
-case 1: without name
-case 2: with too long description
-case 3: invalid data_urls
-"""
 
 
 class TestSellerProductsListView:
@@ -147,7 +142,7 @@ class TestSellerProductsListView:
         assert response.status_code == 200
         assert response.data['status'] == 'OK'
 
-        # checking that all instances are created
+        # checking that all instances were created
         seller_product = SellerProduct.objects.get(name=request_data['name'])
         seller_product_archive = SellerProductArchive.objects.get(name=request_data['name'])
 
@@ -201,4 +196,159 @@ class TestSellerProductsListView:
         assert 'data_urls' in response.data['error']['invalid_fields']
         assert 'url' in response.data['error']['invalid_fields']['data_urls'][0]
         assert 'url' in response.data['error']['invalid_fields']['data_urls'][1]
+
+
+class TestSellerProductsView:
+
+    def test_get(self, data_formats, data_delivery_types):
+        """
+        Test: get specific seller_product
+        """
+        user = mixer.blend(User)
+        seller = mixer.blend(Seller,
+                             seller=user,
+                             logo=None)
+        seller_product = mixer.blend(SellerProduct,
+                                     seller=seller,
+                                     name='seller_product_1',
+                                     descr='seller_product_1_description',
+                                     price_per_one_time=1.1,
+                                     price_per_month=12.1,
+                                     price_per_year=123.1,
+                                     price_by_request=True,
+                                     price_per_usage=True,
+                                     price_per_usage_descr="$10 per 1000 requests",
+                                     rating=1.1)
+        seller_product_data_sample = mixer.blend(SellerProductDataSample,
+                                                 seller_product=seller_product,
+                                                 file=File(open('test-files/test-file-1.py', 'rb')),
+                                                 data_delivery_type=data_delivery_types[0],
+                                                 data_format=data_formats[0])
+        seller_product_data_url = mixer.blend(SellerProductDataUrl,
+                                              seller_product=seller_product,
+                                              url='http://127.0.0.1:8000/',
+                                              data_delivery_type=data_delivery_types[0],
+                                              data_format=data_formats[0])
+        url = reverse('SellerProductsView', kwargs={'pk': seller_product.id})
+        request = APIRequestFactory().get(url)
+        force_authenticate(request, user=user)
+        response = SellerProductsView.as_view()(request=request, pk=seller_product.id)
+        assert response.status_code == 200
+        seller_product_dict = model_to_dict(seller_product)
+        for key, value in response.data.items():
+            if key == 'status':
+                assert value == 'OK'
+            elif key == 'seller':
+                assert value['seller_id'] == user.id
+                assert value['name'] == seller.name
+            elif key == 'data_samples':
+                assert value[0]['file_url'] == f'http://testserver/media/{seller_product_data_sample.file}'
+            elif key == 'data_urls':
+                assert value[0]['url'] == seller_product_data_url.url
+            else:
+                assert seller_product_dict[key] == value
+
+    def test_get_not_authenticated(self):
+        """
+        Test: get with not authenticated user
+        """
+        user = mixer.blend(User)
+        url = reverse('SellerProductsView', kwargs={'pk': 1})
+        request = APIRequestFactory().get(url)
+        request.user = user
+        response = SellerProductsView.as_view()(request=request, pk=1)
+        assert response.status_code == 401
+        assert response.data['error']['code'] == 401
+        assert 'msg' in response.data['error']
+        assert response.data['status'] == "ERR"
+
+    def test_put_update_seller_product(self, categories, geo_regions, languages, data_types, data_formats, data_delivery_types):
+        """
+        Test: update seller-product
+        """
+        user = mixer.blend(User)
+        seller = mixer.blend(Seller,
+                             seller=user,
+                             logo=None)
+
+        seller_product = mixer.blend(SellerProduct,
+                                     seller=seller,
+                                     name='seller_product_1',
+                                     descr='seller_product_1_description',
+                                     price_per_one_time=1.1,
+                                     price_per_month=12.1,
+                                     price_per_year=123.1,
+                                     price_by_request=True,
+                                     price_per_usage=True,
+                                     price_per_usage_descr="$10 per 1000 requests",
+                                     rating=1.1)
+        # seller_product_data_sample_1
+        mixer.blend(SellerProductDataSample,
+                    seller_product=seller_product,
+                    file=File(open('test-files/test-file-1.py', 'rb')),
+                    data_delivery_type=data_delivery_types[0],
+                    data_format=data_formats[0])
+        # seller_product_data_url_1
+        mixer.blend(SellerProductDataUrl,
+                    seller_product=seller_product,
+                    url='http://127.0.0.1:8000/',
+                    data_delivery_type=data_delivery_types[0],
+                    data_format=data_formats[0])
+
+        uploaded_file = mixer.blend(
+            UploadedFile,
+            file=File(open('test-files/test-file-2.py', 'rb')),
+            file_name_original='test-file-2',
+            created_by=user)
+
+        request_data = {
+            "name": "Updated product name",
+            "descr": "Updated product description",
+            "price_one_time": 1,
+            "price_per_month": 2,
+            "price_per_year": 3,
+            "price_by_request": True,
+            "price_per_usage": False,
+            "price_per_usage_descr": "",
+            "data_categories_ids": [2],
+            "data_langs_ids": [1],
+            "data_geo_regions_ids": [2],
+            "data_types_ids": [1],
+            "data_formats_ids": [2],
+            "data_delivery_types_ids": [2],
+            "data_urls": [
+                {"data_delivery_type_id": 1, "data_format_id": 1, "url": "http://domain.com/data1.xlsx"},
+                {"data_delivery_type_id": 2, "data_format_id": 2, "url": "http://domain.com/data1.xml"}
+            ],
+            "data_samples": [
+                str(uploaded_file.uuid)
+            ]
+        }
+        url = reverse('SellerProductsView', kwargs={'pk': seller_product.id})
+        request = APIRequestFactory().put(url, data=json.dumps(request_data), content_type='application/json')
+        force_authenticate(request, user=user)
+        response = SellerProductsView.as_view()(request=request, pk=seller_product.id)
+        assert response.status_code == 200
+        assert response.data['status'] == 'OK'
+        # test get seller-product by old name
+        with pytest.raises(SellerProduct.DoesNotExist):
+            SellerProduct.objects.get(name='seller_product_1')
+        # checking that all instances were updated
+        seller_product = SellerProduct.objects.get(name=request_data['name'])
+        seller_product_archive = SellerProductArchive.objects.get(name=request_data['name'])
+
+        seller_product_data_sample = SellerProductDataSample.objects.get(seller_product=seller_product)
+        seller_product_data_sample_archive = SellerProductDataSampleArchive.objects.get(seller_product=seller_product_archive)
+
+        assert seller_product_data_sample in SellerProductDataSample.objects.filter(seller_product=seller_product)
+        assert len(SellerProductDataSample.objects.filter(seller_product=seller_product)) == 1
+        assert seller_product_data_sample.get_filename_without_extension == str(uploaded_file.uuid)
+        assert str(seller_product_data_sample_archive.file).split('/')[1] == str(seller_product_data_sample.file).split('/')[1]
+
+        SellerProductDataUrl.objects.get(seller_product=seller_product, url=request_data['data_urls'][0]['url'])
+        SellerProductDataUrl.objects.get(seller_product=seller_product, url=request_data['data_urls'][1]['url'])
+        SellerProductDataUrlArchive.objects.get(seller_product=seller_product_archive, url=request_data['data_urls'][0]['url'])
+        SellerProductDataUrlArchive.objects.get(seller_product=seller_product_archive, url=request_data['data_urls'][1]['url'])
+
+
 
